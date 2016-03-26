2bdfaed6a316fd011dfa8df57835e1712f209913
Quinten Krijger <qkrijger@gmail.com>
2016-03-26 23:15:26 +0100

    Do the Tag Image task validation sooner, so it does not happen anyway
    Signed-off-by: Quinten Krijger <qkrijger@gmail.com>

--- src/main/groovy/org/stackwork/gradle/docker/tasks/TagImageTask.groovy
+++ src/main/groovy/org/stackwork/gradle/docker/tasks/TagImageTask.groovy
@@ -1,6 +1,8 @@
 package org.stackwork.gradle.docker.tasks
 
 import org.gradle.api.Project
+import org.gradle.api.internal.TaskInternal
+import org.gradle.api.internal.tasks.execution.TaskValidator
 import org.gradle.api.tasks.Exec
 import org.stackwork.gradle.docker.StackworkExtension
 
@@ -12,15 +14,18 @@ class TagImageTask extends Exec {
 
     description = 'Tag the built during "buildImage" with the stackwork { imageName } and project.version.'
     group = 'Stackwork'
+    addValidator(new TaskValidator() {
+      @Override
+      void validate(TaskInternal task, Collection<String> messages) {
+        if (project.version == Project.DEFAULT_VERSION) {
+          messages.add 'No project version defined. Cannot tag image. Please set "project.version".'
+        }
+        if (!getImageName()) {
+          messages.add 'No docker image name defined. Cannot tag image. Please set "stackwork { imageName }".'
+        }
+      }
+    })
 
-    doFirst {
-      if (project.version == Project.DEFAULT_VERSION) {
-        throw new IllegalStateException('No project version defined. Cannot tag image. Please set "project.version".')
-      }
-      if (!getImageName()) {
-        throw new IllegalStateException('No docker image name defined. Cannot tag image. Please set "stackwork { imageName }".')
-      }
-    }
     commandLine 'docker', 'tag', "${-> project.stackwork.imageId}", "${-> getImageName()}:${-> project.version}"
     doLast {
       project.stackwork.fullImageName = "${getImageName()}:${project.version}"

55ce198f34248ff6771365581b3672f8cd0f8863
Wouter (W.) Danes <wouter.danes@ing.nl>
2017-11-08 23:21:40 +0100

    Made StackworkObject#host() a memoized function that is only called when needed. Should fix the plugin in IDEA.

--- src/main/groovy/org/stackwork/gradle/docker/StackworkObject.groovy
+++ src/main/groovy/org/stackwork/gradle/docker/StackworkObject.groovy
@@ -1,5 +1,6 @@
 package org.stackwork.gradle.docker
 
+import groovy.transform.Memoized
 import org.gradle.api.Project
 import org.stackwork.gradle.docker.tasks.DockerComposeRunner
 
@@ -20,7 +21,6 @@ class StackworkObject {
   String imageId
   String fullImageName
   String composeProject
-  String host
   String baseComposeStack
 
   /**
@@ -37,6 +37,36 @@ class StackworkObject {
     buildDir = "${project.buildDir}/stackwork-plugin"
   }
 
+  @Memoized
+  String getHost() {
+    String dockerHost = System.getenv('DOCKER_HOST')
+    if (dockerHost) {
+      // in case of remote connection to docker daemon
+      project.logger.info("DOCKER_HOST system variable with value '$dockerHost' found. Will expose docker " +
+              "container forwarded ports and the DOCKER_HOST to the test classes.")
+      return new URI(dockerHost).host
+    } else {
+      // in case of local docker daemon
+      project.logger.info 'No DOCKER_HOST system variable found, so assumed is you are using a local Docker daemon'
+
+      OutputStream out = new ByteArrayOutputStream()
+      project.exec {
+        // check if Docker for Mac is the locally installed client, because that creates /var/run/docker.sock
+        // instead of setting the DOCKER_HOST environment variable
+        executable '/bin/bash'
+        args '-c', 'docker version --format "{{.Server.KernelVersion}}"'
+        standardOutput = out
+      }
+      if (out.toString().contains('moby')) {
+        project.logger.info 'Concluded you are using Docker for Mac. Will expose 127.0.0.1 as ip and forwarded ports ' +
+                'to the test classes.'
+        return '127.0.0.1'
+      } else {
+        project.logger.info 'Will expose docker container ips and exposed ports to the test classes.'
+        return null
+      }
+    }
+  }
 
   @Override
   String toString() {
--- src/main/groovy/org/stackwork/gradle/docker/StackworkPlugin.groovy
+++ src/main/groovy/org/stackwork/gradle/docker/StackworkPlugin.groovy
@@ -44,8 +44,6 @@ class StackworkPlugin implements Plugin<Project> {
     project.ext.stackwork = stackworkObject
     stackworkObject.dockerComposeRunner = newDockerComposeRunner(project, stackworkObject)
 
-    evaluateEnvironment()
-
     project.configurations {
       stackwork {
         description = 'Docker artifacts to be made available for an image build'
@@ -71,35 +69,6 @@ class StackworkPlugin implements Plugin<Project> {
       if (applyBuildStepWithStack()) {
         stackworkObject.buildDockerComposeRunner = newBuildDockerComposeRunner(project, stackworkObject)
         registerAndOrderBuildStepWithStackTask()
-      }
-    }
-  }
-
-  private void evaluateEnvironment() {
-    String dockerHost = System.getenv('DOCKER_HOST')
-    if (dockerHost) {
-      // in case of remote connection to docker daemon
-      project.logger.info("DOCKER_HOST system variable with value '$dockerHost' found. Will expose docker " +
-              "container forwarded ports and the DOCKER_HOST to the test classes.")
-      stackworkObject.host = new URI(dockerHost).host
-    } else {
-      // in case of local docker daemon
-      project.logger.info 'No DOCKER_HOST system variable found, so assumed is you are using a local Docker daemon'
-
-      OutputStream out = new ByteArrayOutputStream()
-      project.exec {
-        // check if Docker for Mac is the locally installed client, because that creates /var/run/docker.sock
-        // instead of setting the DOCKER_HOST environment variable
-        executable '/bin/bash'
-        args '-c', 'docker version --format "{{.Server.KernelVersion}}"'
-        standardOutput = out
-      }
-      if (out.toString().contains('moby')) {
-        project.logger.info 'Concluded you are using Docker for Mac. Will expose 127.0.0.1 as ip and forwarded ports ' +
-            'to the test classes.'
-        stackworkObject.host = '127.0.0.1'
-      } else {
-        project.logger.info 'Will expose docker container ips and exposed ports to the test classes.'
       }
     }
   }
